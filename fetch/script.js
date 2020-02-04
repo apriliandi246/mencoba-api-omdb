96db316f24368261db93f6d549db627b252da925
apriliandi246 <apriliandi642@gmail.com>
2020-02-04 12:21:07 +0700

    membuat search movies menggunakan fetch

--- fetch/script.js
+++ fetch/script.js
@@ -1,5 +1,56 @@
 // fetch
+const searchButton = document.querySelector('.search-button');
 
+searchButton.addEventListener('click', function () {
+      const inputKeyword = document.querySelector('.input-keyword');
+
+      fetch('http://www.omdbapi.com/?apikey=80dfc363&s=' + inputKeyword.value)
+            .then(response => response.json())
+            .then(response => {
+                  const movies = response.Search;
+                  let cards = '';
+
+                  movies.forEach(m => {
+                        cards += showCards(m);
+                  });
+
+                  const movieContainer = document.querySelector('.movie-container');
+                  movieContainer.innerHTML = cards;
+
+                  // ketika tombol detail movie diklik
+                  const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+                  modalDetailButton.forEach(btn => {
+                        btn.addEventListener('click', function () {
+                              const imdbid = this.dataset.imdbid;
+
+                              fetch('http://www.omdbapi.com/?apikey=80dfc363&i=' + imdbid)
+                                    .then(response => response.json())
+                                    .then(m => {
+                                          const movieDetail = showMovieDetail(m);
+                                          const modalBody = document.querySelector('.modal-body');
+                                          modalBody.innerHTML = movieDetail;
+                                    });
+                        });
+                  });
+            })
+});
+
+
+// fungsi menampilkan daftar film
+function showCards(m) {
+      return `
+            <div class="col-md-4 my-4">
+                  <div class="card">
+                        <img class="card-img-top" src="${m.Poster}" alt="movie-poster">
+                        <div class="card-body">
+                              <h5 class="card-title">${m.Title}</h5>
+                              <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                              <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+                        </div>
+                  </div>
+            </div>
+      `;
+}
 
 
 
